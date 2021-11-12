ce17e3714b74e93003af78d64ebfbb8b48f97f15
Flavia Gallois <fgbis@hotmail.com>
2021-11-12 12:13:08 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,11 @@ Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
 Bookmark.create(comment: "very good list", movie_id: 1, list_id: 1)
-Bookmark.create(comment: "Classic movies of all time", movie_id: 3, list_id: 2)
+Bookmark.create(comment: "very good movie", movie_id: 2, list_id: 1)
+Bookmark.create(comment: "very good time", movie_id: 4, list_id: 1)
+Bookmark.create(comment: "best movie of all times", movie_id: 3, list_id: 2)
+Bookmark.create(comment: "great moment", movie_id: 1, list_id: 2)
+
+List.create(name: 'Best of')
+List.create(name: 'Classics')
+List.create(name: 'Drama')
